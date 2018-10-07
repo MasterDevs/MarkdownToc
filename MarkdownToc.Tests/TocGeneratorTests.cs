95c8e4a17bc8163774a8f0e6fe8741f08ff61c0f
Josh Quintus <josh.quintus@gmail.com>
2018-10-07 19:14:13 -0400

    Take markdown and generate a toc

--- MarkdownToc.Tests/TocGeneratorTests.cs
+++ MarkdownToc.Tests/TocGeneratorTests.cs
@@ -19,5 +19,54 @@ namespace MarkdownToc.Tests
             // Assert
             Assert.Equal(expected, actual);
         }
+        [Fact]
+        public void GenerateToc_BlankInput_BlankOutput()
+        {
+            // Assemble
+            var generator = new TocGenerator();
+            var md = "";
+
+            // Act
+            var actual = generator.GenerateToc(md);
+
+            // Assert
+            Assert.Equal("", actual);
+        }
+
+        [Fact]
+        public void GenerateToc_BalidMarkdownIn_ReturnsToc()
+        {
+            // Assemble
+            var generator = new TocGenerator();
+
+            var md = @"
+# One
+
+Body of the first
+
+## Two
+
+Body of the second
+
+# Another One
+
+Another body
+
+## Two: Another Two
+";
+
+            var expectedToc = @"
+* [One](#one)
+    * [Two](#two)
+* [Another One](#another-one)
+    * [Two: Another Two](#two--another-two)
+".Trim();
+            // Act
+            var actual = generator.GenerateToc(md);
+
+            // Assert
+            Assert.Equal(expectedToc, actual);
+
+        }
     }
 }
--- MarkdownToc/TocGenerator.cs
+++ MarkdownToc/TocGenerator.cs
@@ -1,4 +1,7 @@
-﻿namespace MarkdownToc
+﻿using System;
+using System.Linq;
+
+namespace MarkdownToc
 {
     public class TocGenerator
     {
@@ -9,6 +12,31 @@
                 .Replace("  ", " ")
                 .Replace(":", " ")
                 .Replace(' ', '-');
+        }
+
+        public string GenerateToc(string md)
+        {
+            var tocLines = md
+                .Split(new[] { "\n", "\r", "\r\n" }, StringSplitOptions.None)
+                .Where(line => line.StartsWith("#"))
+                .Select(line => ToTocEntry(line));
+
+            return string.Join("\r\n", tocLines);
+        }
+
+        private string ToTocEntry(string line)
+        {
+            var count = line
+                .TakeWhile(c => c == '#')
+                .Count();
+
+            var cleanLine = line.Trim('#').Trim();
+
+            var indent = new string(' ', (count - 1) * 4);
+
+            var slug = GenerateSlug(cleanLine);
+
+            return $"{indent}* [{cleanLine}](#{slug})";
         }
     }
 }
