5388e79d61048e86846d903462888ea53d7b2a00
Josh Quintus <josh.quintus@gmail.com>
2018-10-07 14:26:57 -0400

    Make initial sln

---
+++ MarkdownToc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkdownToc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
